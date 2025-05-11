e58d1cf9a3648c6ce3dfbb891096a77f628ec4af
Eng. Mahmoud <cls@khaledfathi.com>
2025-05-11 21:51:25 +0300

    29-[11-5-2025]

--- 01-Examples Pojjects/08-Dashboard/assets/js/chartsJS.js
+++ 01-Examples Pojjects/08-Dashboard/assets/js/chartsJS.js
@@ -2,14 +2,14 @@ const ctx1 = document.getElementById("chart-1").getContext("2d");
 const myChart = new Chart(ctx1, {
   type: "polarArea",
   data: {
-    labels: ["Facebook", "Youtube", "Amazon"],
+    labels: ["clothes", "Food", "Spare Parts"],
     datasets: [
       {
         label: "# of Votes",
         data: [600, 800, 1000],
         backgroundColor: [
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 99, 132, 1)",
+          "rgb(1, 103, 88)",
+          "rgb(214, 45, 194)",
           "rgba(255, 206, 86, 1)",
         ],
       },
@@ -24,13 +24,13 @@ const ctx2 = document.getElementById("chart-2").getContext("2d");
 const myChart2 = new Chart(ctx2, {
   type: "bar",
   data: {
-    labels: ["Facebook", "Youtube", "Amazon"],
+    labels: ["Full stack", "PHP", "Angular"],
     datasets: [
       {
         label: "Earning",
         data: [600, 800, 1000],
         backgroundColor: [
-          "rgba(54, 162, 235, 1)",
+          "rgb(64, 207, 131)",
           "rgba(255, 99, 132, 1)",
           "rgba(255, 206, 86, 1)",
         ],
